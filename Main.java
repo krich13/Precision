617ccc18040554b301afb5f1c75b8451ab911a76
Christine Klimenchenko <klimenchenko.kri.ch@gmail.com>
2021-07-19 23:33:17 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        double regularBonus = 0.3;
+        double specialBonus = 0.6;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
+    }
+}
